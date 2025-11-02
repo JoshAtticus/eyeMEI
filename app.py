5ec1a6e3e879f78dbcdbb53dfec1f31927a236d1
JoshAtticus <git@joshatticus.site>
2025-11-02 10:10:22 +0800

    port change

--- app.py
+++ app.py
@@ -533,4 +533,4 @@ def lookup_imei():
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=3001)
